1c3ea788ab6cd13aff3970cf2db3637caa15d106
rustywhite404 <rustywhite404@gmail.com>
2024-12-11 21:26:01 +0900

    [comment] CommandLineRunner 주석 추가

--- src/main/java/com/myreviewservice/myreviewservice/util/DataInitializer.java
+++ src/main/java/com/myreviewservice/myreviewservice/util/DataInitializer.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 @AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
+    //CommandLineRunner : 애플리케이션 초기화 또는 실행 후 특정 작업을 수행하기 위해 사용되는 인터페이스.
+    //시작 후 초기 데이터 설정이나 외부 API 호출, 캐시 초기화 등의 작업에 유용하다.
 
     private final ProductRepository productRepository;
     @Override
