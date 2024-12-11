9051fb3757092528f7db119956fb4c189592c56b
rustywhite404 <rustywhite404@gmail.com>
2024-12-11 11:01:07 +0900

    init: 초기 SpringBoot 환경 세팅

---
+++ src/main/java/com/myreviewservice/myreviewservice/MyReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myreviewservice.myreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myreviewservice/myreviewservice/MyReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreviewservice.myreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
